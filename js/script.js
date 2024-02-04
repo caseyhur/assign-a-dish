b52a2cabfb3a5060f86deef2727388c789781dca
casey_h <caseyehurlburt@gmail.com>
2024-02-04 13:10:49 -0500

    Items are assigned but need to fix problem of repetition.

--- js/script.js
+++ js/script.js
@@ -10,3 +10,63 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+
+// My JS code
+// only appears when guest list is full
+const assignButton = document.querySelector(".assign");
+// targets the list that will populate with the guest's name and assigned dish
+const assignedItems = document.querySelector(".assigned-items");
+
+addGuestButton.addEventListener("click", function () {
+    const guest = guestInput.value;
+    // console.log(guest);
+    if (guest !== "") {
+        addToList(guest);
+        updateGuestCount();
+        clearInput();
+    }
+});
+
+const clearInput = function () {
+    guestInput.value = "";
+};
+
+const addToList = function (guest) {
+    const listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
+};
+
+const updateGuestCount = function () {
+    const guests = document.querySelectorAll(".guest-list li");
+    guestCount.innerText = guests.length;
+
+    if (guests.length === 8) {
+        addGuestButton.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestFull.classList.remove("hide");
+    }
+};
+
+// assign dish code
+
+const assignItems = function () {
+    const potluckItems = ["potato sald", "baked cauliflower", "short rib", "kimchi salad", "bacon-wrapped dates", "veggie nori rolls", "cheese board", "zucchini noodles", "chilled cucumber noodles", "quinoa salad", "pesto salad", "fruit salad"];
+    
+    const allGuests = document.querySelectorAll(".guest-list li");
+    
+    for (let guest of allGuests) {
+        let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+        let randomPotluckItem = potluckItems[randomPotluckIndex];
+        
+        let listItem = document.createElement("li");
+        listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+        assignedItems.append(listItem);
+    }
+    
+}
+
+assignButton.addEventListener("click", function () {
+    assignItems();
+})
